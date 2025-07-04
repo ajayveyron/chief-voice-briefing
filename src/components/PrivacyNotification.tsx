ac082593d101369c5f8641566efeee3c744a66e5
ajayveyron <ajayveyron9@gmail.com>
2025-07-04 22:53:19 +0530

    Add dismiss button to Privacy Notification component

--- src/components/PrivacyNotification.tsx
+++ src/components/PrivacyNotification.tsx
@@ -308,6 +308,14 @@ const PrivacyNotification: React.FC<PrivacyNotificationProps> = ({
             <ExternalLink className="h-4 w-4 mr-2" />
             Terms & Conditions
           </Button>
+          <Button
+            onClick={handleDismiss}
+            variant="ghost"
+            size="sm"
+            className="flex-1 text-gray-500 hover:text-gray-700"
+          >
+            X
+          </Button>
         </div>
       </CardContent>
     </Card>
